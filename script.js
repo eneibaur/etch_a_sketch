a3e704106fca3c55311f5907d3eece5287f94756
eneibaur <107266929+eneibaur@users.noreply.github.com>
2022-12-20 08:20:07 -0800

    Added `pixel` class to created DIVS

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ let row = 1;
 let column = 1;
 for (let i = 1; i <= (drawSize * drawSize); i++) {
   let cell = document.createElement('div');
-  cell.style.border = '1px solid black';
   cell.style.gridRow = row;
   cell.style.gridColumn = column;
+  cell.classList.add('pixel');
   column += 1;
   if (column === drawSize + 1) {
     row += 1;
