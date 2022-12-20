ef006a396236d4fe2b43411239e58f0256efd1c0
eneibaur <107266929+eneibaur@users.noreply.github.com>
2022-12-20 07:56:09 -0800

    Created initial javascript grid loop

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const testH1 = document.querySelector(".test")
-testH1.style.color = "blue";
+// const testH1 = document.querySelector(".test")
+// testH1.style.color = "blue";
+
+const drawSpace = document.querySelector(".etch");
+const pixel = document.createElement('div');
+pixel.classList.add('pixel')
+
+let startingSize = 16;
+
+
+for (let i = 0; i < (startingSize * startingSize); i++){
+    drawSpace.appendChild(pixel);
+}
